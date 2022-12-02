b7abdc6ca3f2e4c41c4d067df2b4c385210e9d67
Mushi0 <zhy_980401@outlook.com>
2022-12-02 10:59:28 +0000

    Day 2

---
+++ Cpp/Q_2_1/main.cpp
@@ -0,0 +1,73 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int shapeScore(char move){
+    switch(move){
+        case 'X':
+            return 1;
+        case 'Y':
+            return 2;
+        case 'Z':
+            return 3;
+    }
+    return 0;
+}
+
+int resultScore(char myMove, char theirMove){
+    switch(myMove){
+        case 'X':
+            switch(theirMove){
+                case 'A':
+                    return 3;
+                case 'B':
+                    return 0;
+                case 'C':
+                    return 6;
+            }
+        case 'Y':
+            switch(theirMove){
+                case 'A':
+                    return 6;
+                case 'B':
+                    return 3;
+                case 'C':
+                    return 0;
+            }
+        case 'Z':
+            switch(theirMove){
+                case 'A':
+                    return 0;
+                case 'B':
+                    return 6;
+                case 'C':
+                    return 3;
+            }
+    }
+    return 0;
+}
+
+int main(){
+    std::string myText;
+    std::ifstream myInputFile{"..\\..\\Data\\Q2.txt"};
+    char myMove{};
+    char theirMove{};
+    int totalScore{0};
+
+    if (!myInputFile)
+    {
+        std::cerr << "Uh oh, file could not be opened for reading!\n";
+        return 1;
+    }
+
+    while (std::getline(myInputFile, myText))
+    {
+        theirMove = myText[0];
+        myMove = myText[2];
+        totalScore += (shapeScore(myMove) + resultScore(myMove, theirMove));
+    }
+
+    std::cout << "My total score is: " << totalScore;
+
+    return 0;
+}
---
+++ Cpp/Q_2_2/main.cpp
@@ -0,0 +1,73 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int resultScore(char move){
+    switch(move){
+        case 'X':
+            return 0;
+        case 'Y':
+            return 3;
+        case 'Z':
+            return 6;
+    }
+    return 0;
+}
+
+int shapeScore(char myMove, char theirMove){
+    switch(theirMove){
+        case 'A':
+            switch(myMove){
+                case 'X':
+                    return 3;
+                case 'Y':
+                    return 1;
+                case 'Z':
+                    return 2;
+            }
+        case 'B':
+            switch(myMove){
+                case 'X':
+                    return 1;
+                case 'Y':
+                    return 2;
+                case 'Z':
+                    return 3;
+            }
+        case 'C':
+            switch(myMove){
+                case 'X':
+                    return 2;
+                case 'Y':
+                    return 3;
+                case 'Z':
+                    return 1;
+            }
+    }
+    return 0;
+}
+
+int main(){
+    std::string myText;
+    std::ifstream myInputFile{"..\\..\\Data\\Q2.txt"};
+    char myMove{};
+    char theirMove{};
+    int totalScore{0};
+
+    if (!myInputFile)
+    {
+        std::cerr << "Uh oh, file could not be opened for reading!\n";
+        return 1;
+    }
+
+    while (std::getline(myInputFile, myText))
+    {
+        theirMove = myText[0];
+        myMove = myText[2];
+        totalScore += (shapeScore(myMove, theirMove) + resultScore(myMove));
+    }
+
+    std::cout << "My total score is: " << totalScore;
+
+    return 0;
+}
